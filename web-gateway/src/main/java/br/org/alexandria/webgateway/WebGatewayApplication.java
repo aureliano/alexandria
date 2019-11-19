52ef1a284098673bbac1264d23d05c96d84d6ad6
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-19 14:29:38 -0300

    Inicia projeto web-gateway.

---
+++ web-gateway/src/main/java/br/org/alexandria/webgateway/WebGatewayApplication.java
@@ -0,0 +1,12 @@
+package br.org.alexandria.webgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGatewayApplication {
+
+  public static void main (String[] args) {
+    SpringApplication.run (WebGatewayApplication.class, args);
+  }
+}
---
+++ web-gateway/src/test/java/br/org/alexandria/webgateway/WebGatewayApplicationTest.java
@@ -0,0 +1,12 @@
+package br.org.alexandria.webgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGatewayApplicationTests {
+
+  @Test
+  void contextLoads () {
+  }
+}
