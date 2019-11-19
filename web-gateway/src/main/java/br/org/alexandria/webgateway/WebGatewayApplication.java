bfe44fe0fdf73f69b0a274e5147fa59d2d34ec8d
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-19 14:50:57 -0300

    Configura zuul web proxy.

--- web-gateway/src/main/java/br/org/alexandria/webgateway/WebGatewayApplication.java
+++ web-gateway/src/main/java/br/org/alexandria/webgateway/WebGatewayApplication.java
@@ -2,8 +2,10 @@ package br.org.alexandria.webgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class WebGatewayApplication {
 
   public static void main (String[] args) {
