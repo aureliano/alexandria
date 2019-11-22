b081e2798c88892a78884757f620091ee102638b
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-22 18:14:35 -0300

    Implementa serviço para criação do papel de usuário.

--- web-security/src/main/java/br/org/alexandria/websecurity/controller/RoleController.java
+++ web-security/src/main/java/br/org/alexandria/websecurity/controller/RoleController.java
@@ -1,11 +1,18 @@
 package br.org.alexandria.websecurity.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.org.alexandria.websecurity.domain.Role;
+import br.org.alexandria.websecurity.dto.RoleDTO;
+import br.org.alexandria.websecurity.helper.WebHelper;
 import br.org.alexandria.websecurity.repository.RoleRepository;
 
 @Controller
@@ -17,5 +24,20 @@ public class RoleController {
   @GetMapping(path = "/roles", produces = { "application/json" })
   public @ResponseBody Iterable<Role> roles () {
     return this.roleRepository.findAll ();
+  }
+
+  @PostMapping(path = "/roles", consumes = "application/json", produces = {
+      "application/json" })
+  public @ResponseBody ResponseEntity<RoleDTO> rolesNew (
+      @RequestBody RoleDTO dto) {
+    Role role = new Role ();
+    role.setName (dto.getRole ());
+    role.setDescription (dto.getDescription ());
+
+    this.roleRepository.save (role);
+    dto.setId (role.getId ());
+
+    URI uri = WebHelper.toURI ("/roles/" + dto.getId ());
+    return ResponseEntity.created (uri).body (dto);
   }
 }
