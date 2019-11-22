515565a6a698e7c045ee62156bd05a35bebc0a63
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-22 18:14:02 -0300

    Trata exceções do tipo violação de restrição.

---
+++ web-security/src/main/java/br/org/alexandria/websecurity/exception/WebExceptionHandler.java
@@ -0,0 +1,25 @@
+package br.org.alexandria.websecurity.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.org.alexandria.websecurity.helper.WebHelper;
+
+@ControllerAdvice
+public class WebExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(value = { DataIntegrityViolationException.class })
+  protected ResponseEntity<Object> handleConflict (RuntimeException ex,
+      WebRequest request) {
+
+    Throwable rootCause = WebHelper.findCause (ex);
+    return handleExceptionInternal (ex, rootCause.getMessage (),
+        new HttpHeaders (), HttpStatus.CONFLICT, request);
+  }
+}
