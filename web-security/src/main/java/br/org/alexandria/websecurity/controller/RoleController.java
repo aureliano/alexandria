055af3cea940550bd723300ebc638e3916d62809
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-25 12:21:59 -0300

    Exclusão de papel de usuário.

--- web-security/src/main/java/br/org/alexandria/websecurity/controller/RoleController.java
+++ web-security/src/main/java/br/org/alexandria/websecurity/controller/RoleController.java
@@ -4,9 +4,11 @@ import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +20,16 @@ import br.org.alexandria.websecurity.domain.Role;
 import br.org.alexandria.websecurity.dto.RoleDTO;
 import br.org.alexandria.websecurity.helper.WebHelper;
 import br.org.alexandria.websecurity.repository.RoleRepository;
+import br.org.alexandria.websecurity.service.RoleService;
 
 @Controller
 public class RoleController {
 
   @Autowired
   private RoleRepository roleRepository;
+
+  @Autowired
+  private RoleService roleService;
 
   @GetMapping(path = "/roles", produces = { "application/json" })
   public @ResponseBody Iterable<Role> roles () {
@@ -45,8 +51,7 @@ public class RoleController {
     return ResponseEntity.created (uri).body (dto);
   }
 
-  @PutMapping(path = "/roles/{id}", consumes = "application/json", produces = {
-      "application/json" })
+  @PutMapping(path = "/roles/{id}", consumes = "application/json")
   public @ResponseBody ResponseEntity<RoleDTO> rolesUpdate (
       @PathVariable Long id, @RequestBody RoleDTO dto) {
     Optional<Role> optional = this.roleRepository.findById (id);
@@ -60,6 +65,25 @@ public class RoleController {
 
     this.roleRepository.save (role);
     dto.setId (role.getId ());
+
+    return ResponseEntity.status (HttpStatus.NO_CONTENT).build ();
+  }
+
+  @DeleteMapping(path = "/roles/{id}", produces = { "application/json" })
+  public @ResponseBody ResponseEntity<RoleDTO> rolesDelete (
+      @PathVariable Long id) {
+    Optional<Role> optional = this.roleRepository.findById (id);
+    if (!optional.isPresent ()) {
+      return ResponseEntity.notFound ().build ();
+    }
+
+    Role role = optional.get ();
+    if (this.roleService.countUserRoles (id) > 0) {
+      String msg = "You cannot delete a role which still has users related to it.";
+      throw new DataIntegrityViolationException (msg);
+    }
+
+    this.roleRepository.delete (role);
 
     return ResponseEntity.status (HttpStatus.NO_CONTENT).build ();
   }
