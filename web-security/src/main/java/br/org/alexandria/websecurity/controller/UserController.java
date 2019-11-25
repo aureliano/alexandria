5b404ee6286ab983a6eb97b8cdcb748669a00f80
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-25 18:02:21 -0300

    Refatora crud de usuários.

--- web-security/src/main/java/br/org/alexandria/websecurity/controller/UserController.java
+++ web-security/src/main/java/br/org/alexandria/websecurity/controller/UserController.java
@@ -4,10 +4,13 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,13 +24,13 @@ public class UserController {
   @Autowired
   private UserService userService;
 
-  @GetMapping(path = "/users", produces = { "application/json" })
+  @GetMapping(path = "/v1/users", produces = { "application/json" })
   public @ResponseBody ResponseEntity<List<UserDTO>> users () {
     final List<UserDTO> users = this.userService.findAllUsersDTO ();
     return ResponseEntity.accepted ().body (users);
   }
 
-  @PostMapping(path = "/users", consumes = "application/json", produces = {
+  @PostMapping(path = "/v1/users", consumes = "application/json", produces = {
       "application/json" })
   public @ResponseBody ResponseEntity<UserDTO> rolesNew (
       @RequestBody UserDTO dto) {
@@ -36,5 +39,12 @@ public class UserController {
 
     URI uri = WebHelper.toURI ("/users/" + dto.getId ());
     return ResponseEntity.created (uri).body (dto);
+  }
+
+  @PutMapping(path = "/v1/users/{id}", consumes = "application/json")
+  public @ResponseBody ResponseEntity<UserDTO> rolesUpdate (
+      @PathVariable Long id, @RequestBody UserDTO dto) {
+    this.userService.updateUser (id, dto);
+    return ResponseEntity.status (HttpStatus.NO_CONTENT).build ();
   }
 }
--- web-security/src/main/java/br/org/alexandria/websecurity/service/UserService.java
+++ web-security/src/main/java/br/org/alexandria/websecurity/service/UserService.java
@@ -3,18 +3,19 @@ package br.org.alexandria.websecurity.service;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import br.org.alexandria.websecurity.domain.Role;
 import br.org.alexandria.websecurity.domain.User;
 import br.org.alexandria.websecurity.dto.UserDTO;
 import br.org.alexandria.websecurity.exception.WebSecurityException;
+import br.org.alexandria.websecurity.helper.SecurityHelper;
 import br.org.alexandria.websecurity.repository.UserRepository;
 
 @Service
@@ -22,12 +23,6 @@ public class UserService {
 
   @Autowired
   private UserRepository userRepository;
-
-  private PasswordEncoder passwordEncoder;
-
-  public UserService () {
-    this.passwordEncoder = new BCryptPasswordEncoder ();
-  }
 
   public List<UserDTO> findAllUsersDTO () {
     final List<UserDTO> users = new ArrayList<UserDTO> ();
@@ -55,14 +50,27 @@ public class UserService {
   public Long registerUser (UserDTO dto) {
     if (dto.getPassword () != null
         && !dto.getPassword ().equals (dto.getConfirmPassword ())) {
-      throw new WebSecurityException ("Password != confirmedPassword");
+      throw new WebSecurityException ("Password != confirmedPassword",
+          HttpStatus.BAD_REQUEST);
     }
 
     User user = new User ();
     user.setName (dto.getName ());
-    user.setPassword (this.passwordEncoder.encode (dto.getPassword ()));
+    user.setPassword (SecurityHelper.encodePassword (dto.getPassword ()));
 
     this.userRepository.save (user);
     return user.getId ();
+  }
+
+  public void updateUser (Long id, UserDTO dto) {
+    Optional<User> optional = this.userRepository.findById (id);
+    if (!optional.isPresent ()) {
+      throw new WebSecurityException ("User not found.", HttpStatus.NOT_FOUND);
+    }
+
+    User user = new User ();
+    user.setName (dto.getName ());
+
+    this.userRepository.save (user);
   }
 }
