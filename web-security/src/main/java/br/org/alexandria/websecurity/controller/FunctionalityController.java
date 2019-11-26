e2ce3f7a1d5a6f58534274073f10f219d8afa035
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-26 15:32:03 -0300

    Serviço para listagem de funcionalidades.

--- web-security/src/main/java/br/org/alexandria/websecurity/controller/FunctionalityController.java
+++ web-security/src/main/java/br/org/alexandria/websecurity/controller/FunctionalityController.java
@@ -1,8 +1,14 @@
 package br.org.alexandria.websecurity.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.org.alexandria.websecurity.dto.FunctionalityDTO;
 import br.org.alexandria.websecurity.helper.WebHelper;
 import br.org.alexandria.websecurity.service.FunctionalityService;
 
@@ -15,4 +21,10 @@ public class FunctionalityController {
   @Autowired
   private WebHelper webHelper;
 
+  @GetMapping(path = "/v1/functionalities", produces = { "application/json" })
+  public @ResponseBody ResponseEntity<List<FunctionalityDTO>> roles () {
+    final List<FunctionalityDTO> functionalities = this.functionalityService
+        .findAllFunctionalitiesDTO ();
+    return ResponseEntity.accepted ().body (functionalities);
+  }
 }
--- web-security/src/main/java/br/org/alexandria/websecurity/service/FunctionalityService.java
+++ web-security/src/main/java/br/org/alexandria/websecurity/service/FunctionalityService.java
@@ -1,8 +1,12 @@
 package br.org.alexandria.websecurity.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.org.alexandria.websecurity.dto.FunctionalityDTO;
 import br.org.alexandria.websecurity.repository.FunctionalityRepository;
 
 @Service
@@ -10,4 +14,17 @@ public class FunctionalityService {
 
   @Autowired
   private FunctionalityRepository functionalityRepository;
+
+  public List<FunctionalityDTO> findAllFunctionalitiesDTO () {
+    final List<FunctionalityDTO> functionalities = new ArrayList<> ();
+    this.functionalityRepository.findAll ().forEach (f -> {
+      FunctionalityDTO dto = new FunctionalityDTO ();
+      dto.setId (f.getId ());
+      dto.setFunctionality (f.getName ());
+
+      functionalities.add (dto);
+    });
+
+    return functionalities;
+  }
 }
