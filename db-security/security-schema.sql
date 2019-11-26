6e3a04509ed9ae95a51cf51019985f48fc9c3191
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-26 15:18:37 -0300

    Atualiza esquema de dados.

--- db-security/security-schema.sql
+++ db-security/security-schema.sql
@@ -2,7 +2,14 @@ CREATE TABLE user_roles(
   id BIGINT NOT NULL PRIMARY KEY,
   name VARCHAR(25) NOT NULL,
   description VARCHAR(100) NOT NULL,
-  CONSTRAINT name_unique UNIQUE(name)
+  CONSTRAINT role_name_unique UNIQUE(name)
+);
+
+CREATE TABLE functionalities(
+  id BIGINT NOT NULL PRIMARY KEY,
+  name VARCHAR(25) NOT NULL,
+  description VARCHAR(100) NOT NULL,
+  CONSTRAINT functionality_name_unique UNIQUE(name)
 );
 
 CREATE TABLE users(
@@ -18,5 +25,13 @@ CREATE TABLE user_roles_users(
   CONSTRAINT users_fk FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
+CREATE TABLE roles_functionalities(
+  role_id BIGINT NOT NULL,
+  functionality_id BIGINT NOT NULL,
+  CONSTRAINT user_roles_fk FOREIGN KEY(role_id) REFERENCES user_roles(id),
+  CONSTRAINT functionalities_fk FOREIGN KEY(functionality_id) REFERENCES functionalities(id)
+);
+
 CREATE SEQUENCE user_roles_seq START WITH 1 INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 NO CYCLE;
 CREATE SEQUENCE users_seq START WITH 1 INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 NO CYCLE;
+CREATE SEQUENCE functionalities_seq START WITH 1 INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 NO CYCLE;
