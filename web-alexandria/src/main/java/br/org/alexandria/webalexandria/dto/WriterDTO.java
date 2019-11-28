5324bdab4d297f3078fe1c8a93d0bdde476a4ee7
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-28 17:09:59 -0300

    Altera tipo do id de long para Long.

--- web-alexandria/src/main/java/br/org/alexandria/webalexandria/dto/WriterDTO.java
+++ web-alexandria/src/main/java/br/org/alexandria/webalexandria/dto/WriterDTO.java
@@ -6,16 +6,16 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @JsonInclude(Include.NON_NULL)
 public class WriterDTO {
 
-  private long id;
+  private Long id;
   private String fullName;
   private String nickName;
   private String nationality;
 
-  public long getId () {
+  public Long getId () {
     return id;
   }
 
-  public void setId (long id) {
+  public void setId (Long id) {
     this.id = id;
   }
 
