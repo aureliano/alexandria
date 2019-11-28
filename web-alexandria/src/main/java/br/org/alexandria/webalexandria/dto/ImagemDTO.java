6884abbd5108fca1da8cf2756964087a5b5c80c0
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-28 17:18:47 -0300

    DTO image.

---
+++ web-alexandria/src/main/java/br/org/alexandria/webalexandria/dto/ImagemDTO.java
@@ -0,0 +1,27 @@
+package br.org.alexandria.webalexandria.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class ImagemDTO {
+
+  private Long id;
+  private String filePath;
+
+  public Long getId () {
+    return id;
+  }
+
+  public void setId (Long id) {
+    this.id = id;
+  }
+
+  public String getFilePath () {
+    return filePath;
+  }
+
+  public void setFilePath (String filePath) {
+    this.filePath = filePath;
+  }
+}
