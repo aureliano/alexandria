ceb844b59883206f63905fe62a58da823dca207f
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-28 16:07:20 -0300

    Banco de dados do microsserviço web-alexandria.

---
+++ db-alexandria/run-container.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+dir=$(dirname "${0}")
+if [ "${dir}" = '.' ];
+then
+  dir=$(pwd)
+fi
+
+container=$(basename "${dir}")
+
+pgpwd=$1
+image="${container}:latest"
+bind_address='127.0.0.1:5434:5432'
+volume='alexandria_alx_pgdata'
+
+docker volume create "${volume}"
+volume="${volume}:/var/lib/postgresql/data"
+
+if [ "${pgpwd}Z" = 'Z' ];
+then
+  pgpwd='postgres'
+fi
+
+docker run --rm -P \
+  --name "${container}" \
+  -p "${bind_address}" \
+  -e POSTGRES_PASSWORD="${pgpwd}" \
+  -v "${volume}" \
+  -d "${image}"
