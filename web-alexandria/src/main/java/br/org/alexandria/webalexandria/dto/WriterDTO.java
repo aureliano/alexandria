d15e24e51b1c0cf31d888e3bcbb05f3cc079c19e
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-28 17:03:31 -0300

    DTO writer.

---
+++ web-alexandria/src/main/java/br/org/alexandria/webalexandria/dto/WriterDTO.java
@@ -0,0 +1,45 @@
+package br.org.alexandria.webalexandria.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class WriterDTO {
+
+  private long id;
+  private String fullName;
+  private String nickName;
+  private String nationality;
+
+  public long getId () {
+    return id;
+  }
+
+  public void setId (long id) {
+    this.id = id;
+  }
+
+  public String getFullName () {
+    return fullName;
+  }
+
+  public void setFullName (String fullName) {
+    this.fullName = fullName;
+  }
+
+  public String getNickName () {
+    return nickName;
+  }
+
+  public void setNickName (String nickName) {
+    this.nickName = nickName;
+  }
+
+  public String getNationality () {
+    return nationality;
+  }
+
+  public void setNationality (String nationality) {
+    this.nationality = nationality;
+  }
+}
