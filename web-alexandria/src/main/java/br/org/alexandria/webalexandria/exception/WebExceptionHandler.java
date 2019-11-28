c18b2684ec55546f877f472416b74061e2df333b
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-28 19:05:10 -0300

    Exception handler.

---
+++ web-alexandria/src/main/java/br/org/alexandria/webalexandria/exception/WebExceptionHandler.java
@@ -0,0 +1,50 @@
+package br.org.alexandria.webalexandria.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.org.alexandria.commons.helper.ExceptionHelper;
+
+@ControllerAdvice
+public class WebExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @Autowired
+  private ExceptionHelper exceptionHelper;
+
+  private static final Logger logger = LoggerFactory
+      .getLogger (WebExceptionHandler.class);
+
+  @ExceptionHandler(value = { DataIntegrityViolationException.class })
+  protected ResponseEntity<Object> handleConflict (RuntimeException ex,
+      WebRequest request) {
+
+    Throwable rootCause = this.exceptionHelper.findCause (ex);
+    HttpStatus status = HttpStatus.CONFLICT;
+    logger.error (ex.getMessage (), ex);
+
+    return handleExceptionInternal (ex, rootCause.getMessage (),
+        new HttpHeaders (), status, request);
+  }
+
+  @ExceptionHandler(value = { WebAlexandriaException.class })
+  protected ResponseEntity<Object> handleWebSecurityException (
+      WebAlexandriaException ex, WebRequest request) {
+
+    Throwable rootCause = this.exceptionHelper.findCause (ex);
+    logger.warn ("Handled exception: {}\nCause: {}\nResponse code: {}",
+        ex.getClass ().getName (), rootCause.getMessage (),
+        ex.getHttpStatus ());
+
+    return handleExceptionInternal (ex, rootCause.getMessage (),
+        new HttpHeaders (), ex.getHttpStatus (), request);
+  }
+}
