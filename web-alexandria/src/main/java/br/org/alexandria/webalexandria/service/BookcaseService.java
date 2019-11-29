e51f6c0a76355ce9d4f744b409c76aaf78b639aa
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-29 15:27:14 -0300

    Bookcase service.

---
+++ web-alexandria/src/main/java/br/org/alexandria/webalexandria/service/BookcaseService.java
@@ -0,0 +1,11 @@
+package br.org.alexandria.webalexandria.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookcaseService {
+
+  @Autowired
+  private BookcaseService bookcaseService;
+}
