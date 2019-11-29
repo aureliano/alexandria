46c3c478f8154a17f0b5af4abb71cf0dfa8d3f2c
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-29 15:51:11 -0300

    Configura bean webHelper.

--- web-alexandria/src/main/java/br/org/alexandria/webalexandria/configuration/BeansConfiguration.java
+++ web-alexandria/src/main/java/br/org/alexandria/webalexandria/configuration/BeansConfiguration.java
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.org.alexandria.commons.helper.ExceptionHelper;
+import br.org.alexandria.commons.helper.WebHelper;
 
 @Configuration
 public class BeansConfiguration {
+
+  @Bean
+  public WebHelper getWebHelper () {
+    return new WebHelper ();
+  }
 
   @Bean
   public ExceptionHelper getExceptionHelper () {
