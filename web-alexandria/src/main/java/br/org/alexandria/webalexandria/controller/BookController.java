de88a7075b8e32106e4894ac54bfce9683953947
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-29 17:16:14 -0300

    Serviço de listagem de livros.

--- web-alexandria/src/main/java/br/org/alexandria/webalexandria/controller/BookController.java
+++ web-alexandria/src/main/java/br/org/alexandria/webalexandria/controller/BookController.java
@@ -1,9 +1,15 @@
 package br.org.alexandria.webalexandria.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.org.alexandria.commons.helper.WebHelper;
+import br.org.alexandria.webalexandria.dto.BookDTO;
+import br.org.alexandria.webalexandria.dto.PageDTO;
 import br.org.alexandria.webalexandria.service.BookService;
 
 @Controller
@@ -14,4 +20,13 @@ public class BookController {
 
   @Autowired
   private WebHelper webHelper;
+
+  @GetMapping(path = "/api/v1/books", params = { "page", "size" }, produces = {
+      "application/json" })
+  public @ResponseBody ResponseEntity<PageDTO<BookDTO>> users (
+      @RequestParam int page, @RequestParam int size) {
+    final PageDTO<BookDTO> books = this.bookService.findAllBooksDTO (page,
+        size);
+    return ResponseEntity.accepted ().body (books);
+  }
 }
--- web-alexandria/src/main/java/br/org/alexandria/webalexandria/service/BookService.java
+++ web-alexandria/src/main/java/br/org/alexandria/webalexandria/service/BookService.java
@@ -1,13 +1,55 @@
 package br.org.alexandria.webalexandria.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import br.org.alexandria.webalexandria.domain.Book;
+import br.org.alexandria.webalexandria.dto.BookDTO;
+import br.org.alexandria.webalexandria.dto.PageDTO;
+import br.org.alexandria.webalexandria.exception.WebAlexandriaException;
 import br.org.alexandria.webalexandria.repository.BookRepository;
 
 @Service
 public class BookService {
 
+  private static final Integer MAX_PAGE_SIZE = 20;
+
   @Autowired
   private BookRepository bookRepository;
+
+  public PageDTO<BookDTO> findAllBooksDTO (int pageNum, int pagesize) {
+    if (pagesize > MAX_PAGE_SIZE) {
+      String msg = String.format ("Page size must be at most %d.",
+          MAX_PAGE_SIZE);
+      throw new WebAlexandriaException (msg, HttpStatus.BAD_REQUEST);
+    }
+
+    Pageable pageable = PageRequest.of (pageNum, pagesize);
+    Page<Book> page = this.bookRepository.findAll (pageable);
+
+    final List<BookDTO> books = new ArrayList<> (page.getNumberOfElements ());
+    page.getContent ().forEach (b -> {
+      BookDTO dto = new BookDTO ();
+      dto.setId (b.getId ());
+      dto.setTitle (b.getTitle ());
+
+      books.add (dto);
+    });
+
+    PageDTO<BookDTO> pageDTO = new PageDTO<> ();
+    pageDTO.setData (books);
+    pageDTO.setPageNumber (page.getNumber ());
+    pageDTO.setPageSize (page.getSize ());
+    pageDTO.setTotalElements (page.getTotalElements ());
+    pageDTO.setTotalPages (page.getTotalPages ());
+
+    return pageDTO;
+  }
 }
