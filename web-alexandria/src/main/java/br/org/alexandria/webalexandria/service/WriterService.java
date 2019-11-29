d34fb1a5189bca4c8d465cc814faaf234f6f6807
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2019-11-29 15:25:41 -0300

    Writer service.

---
+++ web-alexandria/src/main/java/br/org/alexandria/webalexandria/service/WriterService.java
@@ -0,0 +1,12 @@
+package br.org.alexandria.webalexandria.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WriterService {
+
+  @Autowired
+  private WriterService writerService;
+
+}
